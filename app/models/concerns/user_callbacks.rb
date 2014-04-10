8a88e18149f756bb66a8d72a03e5a1a1e200051a
Tad Preston <tpreston@udsamerica.com>
2014-04-10 15:41:01 -0500

    Validation of password only if password present

--- app/models/concerns/user_callbacks.rb
+++ app/models/concerns/user_callbacks.rb
@@ -4,24 +4,26 @@ class UserCallbacks
   end
 
   def self.before_validation(user)
-    if user.oauth
-      oauth_hash = OauthHash.new(user.oauth).to_json
+    unless user.persisted?
+      if user.oauth
+        oauth_hash = OauthHash.new(user.oauth).to_json
 
-      user.provider = oauth_hash["provider"]
-      user.uid = user.oauth["uid"]
-      user.email = oauth_hash["email"]
-      user.username = oauth_hash["username"]
-      user.first_name = oauth_hash["first_name"]
-      user.last_name = oauth_hash["last_name"]
-      user.location = oauth_hash["location"]
-      user.time_zone = oauth_hash["timezone"]
-      user.locale = oauth_hash["locale"]
-      user.profile_image = oauth_hash["profile_image"]
-      tmp_pwd = SecureRandom.hex
-      user.password = tmp_pwd
-      user.password_confirmation = tmp_pwd
-    else
-      user.provider = "capsule"
+        user.provider = oauth_hash["provider"]
+        user.uid = user.oauth["uid"]
+        user.email = oauth_hash["email"]
+        user.username = oauth_hash["username"]
+        user.first_name = oauth_hash["first_name"]
+        user.last_name = oauth_hash["last_name"]
+        user.location = oauth_hash["location"]
+        user.time_zone = oauth_hash["timezone"]
+        user.locale = oauth_hash["locale"]
+        user.profile_image = oauth_hash["profile_image"]
+        tmp_pwd = SecureRandom.hex
+        user.password = tmp_pwd
+        user.password_confirmation = tmp_pwd
+      else
+        user.provider = "capsule"
+      end
     end
   end
 end
