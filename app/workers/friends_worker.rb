58b80c48c0adfbf470c91b2a7ec2ae23ad36c99c
Tad Preston <tpreston@udsamerica.com>
2014-04-28 16:08:38 -0500

    Check to see if there is a friends array

--- app/workers/friends_worker.rb
+++ app/workers/friends_worker.rb
@@ -5,13 +5,15 @@ class FriendsWorker
     begin
       user = User.find user_id
 
-      friends = eval(user.oauth["friends"])
-      ids = friends.collect { |f| f["id"] }.sort
-      User.where(uid: ids).find_each do |u|
-        user.add_as_contact(u)
-        u.add_as_contact(user)
-        user.follow!(u)
-        u.follow!(user)
+      if user.oauth["friends"]
+        friends = eval(user.oauth["friends"])
+        ids = friends.collect { |f| f["id"] }.sort
+        User.where(uid: ids).find_each do |u|
+          user.add_as_contact(u)
+          u.add_as_contact(user)
+          user.follow!(u)
+          u.follow!(user)
+        end
       end
     rescue ActiveRecord::RecordNotFound
     end
