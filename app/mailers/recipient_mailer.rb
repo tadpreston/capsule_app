9fa92efae9aa7436b0e84f6652024acc85ea5cb4
Tad Preston <tpreston@udsamerica.com>
2014-06-11 15:29:03 -0500

    Sending all recipient emails to me

--- app/mailers/recipient_mailer.rb
+++ app/mailers/recipient_mailer.rb
@@ -4,7 +4,7 @@ class RecipientMailer < ActionMailer::Base
   def capsule_notification(user_id, capsule_id)
     @user = User.find user_id
     @capsule = Capsule.find capsule_id
-    mail(to: @user.email, subject: "Welcome to Capsule! You have been sent a capsule.", content_type: "text/html")
-#   mail(to: 'tadpreston@gmail.com', subject: "Welcome to Capsule! You have been sent a capsule.", content_type: "text/html")
+#   mail(to: @user.email, subject: "Welcome to Capsule! You have been sent a capsule.", content_type: "text/html")
+    mail(to: 'tadpreston@gmail.com', subject: "Welcome to Capsule! You have been sent a capsule.", content_type: "text/html")
   end
 end
