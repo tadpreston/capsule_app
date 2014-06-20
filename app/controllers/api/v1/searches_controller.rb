fa9d5f4e559133ebcf5afd5bb3e68caf75e83dce
Tad Preston <tpreston@udsamerica.com>
2014-06-20 10:43:40 -0500

    Return to real searching

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -10,19 +10,19 @@ module API
         @hashtags = []
         query = params[:query]
 
-#        if query[0] == '#'
-#          @hashtags = Hashtag.where('tag ilike ?', "%#{query.match(/^.?(.*)/)[1]}%").order(:tag)
-#        elsif query.include?('@')
-#          user_query = query.split(' ').select { |s| s.include? '@' }.join
-#          @users = User.where('email = ? OR username = ?',  user_query, user_query.match(/^.?(.*)/)[1])
-#        else
-#          @capsules = Capsule.where('title ilike ?', "%#{query}%")
-#          @hashtags = Hashtag.where('tag ilike ?', "%#{query.match(/^.?(.*)/)[1]}%")
-#          @users = User.where('first_name ilike ? OR last_name ilike ?', "%#{query}%", "%#{query}%")
-#        end
-        @capsules = Capsule.find([4535, 4578, 4700, 4824])
-        @hashtags = Hashtag.find([4036, 4041, 4214, 4326])
-        @users = User.find([153, 250, 623])
+        if query[0] == '#'
+          @hashtags = Hashtag.where('tag ilike ?', "%#{query.match(/^.?(.*)/)[1]}%").order(:tag)
+        elsif query.include?('@')
+          user_query = query.split(' ').select { |s| s.include? '@' }.join
+          @users = User.where('email = ? OR username = ?',  user_query, user_query.match(/^.?(.*)/)[1])
+        else
+          @capsules = Capsule.where('title ilike ?', "%#{query}%")
+          @hashtags = Hashtag.where('tag ilike ?', "%#{query.match(/^.?(.*)/)[1]}%")
+          @users = User.where('first_name ilike ? OR last_name ilike ?', "%#{query}%", "%#{query}%")
+        end
+#        @capsules = Capsule.find([4535, 4578, 4700, 4824])
+#        @hashtags = Hashtag.find([4036, 4041, 4214, 4326])
+#        @users = User.find([153, 250, 623])
       end
     end
 
