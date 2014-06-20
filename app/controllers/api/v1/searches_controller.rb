178573466d81510521d58ceeadaec9539d6e9e67
Tad Preston <tpreston@udsamerica.com>
2014-06-20 15:03:37 -0500

    Filter out hidden and relative location capsules

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -16,13 +16,10 @@ module API
           user_query = query.split(' ').select { |s| s.include? '@' }.join
           @users = User.where('email = ? OR username = ?',  user_query, user_query.match(/^.?(.*)/)[1])
         else
-          @capsules = Capsule.where('title ilike ?', "%#{query}%")
+          @capsules = Capsule.where('title ilike ? AND incognito = ? AND relative_location = NULL', "%#{query}%", false)
           @hashtags = Hashtag.where('tag ilike ?', "%#{query.match(/^.?(.*)/)[1]}%")
           @users = User.where('first_name ilike ? OR last_name ilike ?', "%#{query}%", "%#{query}%")
         end
-#        @capsules = Capsule.find([4535, 4578, 4700, 4824])
-#        @hashtags = Hashtag.find([4036, 4041, 4214, 4326])
-#        @users = User.find([153, 250, 623])
       end
     end
 
