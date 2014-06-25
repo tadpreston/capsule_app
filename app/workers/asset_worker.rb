60a16eb067e8ea3e2c753dada452d0c0e5c83da9
Tad Preston <tpreston@udsamerica.com>
2014-06-25 13:57:17 -0500

    Beginning work on processing images

--- app/workers/asset_worker.rb
+++ app/workers/asset_worker.rb
@@ -16,13 +16,54 @@ class AssetWorker
       storage_path = "#{SecureRandom.urlsafe_base64(32)}/#{asset.resource}"
 
       if source_bucket.objects[asset.resource].exists?
-        unless asset.media_type == 'audio'  # copy image and video objects from upload to storage bucket
+        if asset.media_type == 'video'  # copy image and video objects from upload to storage bucket
           dest_bucket = s3.buckets[ENV['S3_BUCKET']]
           source_obj = source_bucket.objects[asset.resource]
           dest_obj = dest_bucket.objects[storage_path]
 
           source_obj.copy_to(dest_obj, { acl: :public_read })
           asset.update_attributes(resource: storage_path, complete: true)
+        elsif asset.media_type == "image"  #process the image with transloadit
+          transloadit = Transloadit.new(
+            service: ENV['TRANSLOADIT_URL'],
+            key: ENV['TRANSLOADIT_AUTH_KEY'],
+            secret: ENV['TRANSLOADIT_SECRET_KEY']
+          )
+
+          options = {
+            key: ENV['AWS_ACCESS_KEY'],
+            secret: ENV['AWS_SECRET_KEY'],
+            bucket: ENV['S3_BUCKET_UPLOAD'],
+            path: asset.resource
+          }
+          original = transloadit.step('original', '/s3/import', options)
+
+          options = {
+            use: "original",
+            preserve_meta_data: false
+          }
+          optimized = transloadit.step('optimized', '/image/optimize', options)
+
+          options = {
+            use: "original",
+            strip: true,
+
+          }
+          resize = transloadit.step('reduced', '/image/resize', options)
+
+          options = {
+            key: ENV['AWS_ACCESS_KEY'],
+            secret: ENV['AWS_SECRET_KEY'],
+            bucket: ENV['S3_BUCKET'],
+            path: storage_path,
+            use: ["original","resize","optimized"]
+          }
+          store = transloadit.step('store', '/s3/store', options)
+
+          assembly = transloadit.assembly(steps: [original, optimized, resize, store])
+          assembly.submit!
+
+          asset.update_attributes(resource: storage_path)
         else  # process audio with Transloadit
           transloadit = Transloadit.new(
             service: ENV['TRANSLOADIT_URL'],
