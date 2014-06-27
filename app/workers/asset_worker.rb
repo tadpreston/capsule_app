7f2340d6611e1857de9ea6ee88908f9423fd0c91
Tad Preston <tpreston@udsamerica.com>
2014-06-27 14:11:58 -0500

    Refactor of the media processing

--- app/workers/asset_worker.rb
+++ app/workers/asset_worker.rb
@@ -11,113 +11,15 @@ class AssetWorker
         access_key_id: ENV['AWS_ACCESS_KEY'],
         secret_access_key: ENV['AWS_SECRET_KEY']
       )
-
       source_bucket = s3.buckets[ENV['S3_BUCKET_UPLOAD']]
 
       if source_bucket.objects[asset.resource].exists?
-        if asset.media_type == 'video'  # copy image and video objects from upload to storage bucket
-          dest_bucket = s3.buckets[ENV['S3_BUCKET']]
-          source_obj = source_bucket.objects[asset.resource]
-          dest_obj = dest_bucket.objects[storage_path]
-
-          source_obj.copy_to(dest_obj, { acl: :public_read })
-          asset.update_attributes(resource: storage_path, complete: true)
-        elsif asset.media_type == "image"  #process the image with transloadit
-          storage_path = "#{SecureRandom.urlsafe_base64(32)}/"
-
-          transloadit = Transloadit.new(
-            service: ENV['TRANSLOADIT_URL'],
-            key: ENV['TRANSLOADIT_AUTH_KEY'],
-            secret: ENV['TRANSLOADIT_SECRET_KEY']
-          )
-
-          options = {
-            key: ENV['AWS_ACCESS_KEY'],
-            secret: ENV['AWS_SECRET_KEY'],
-            bucket: ENV['S3_BUCKET_UPLOAD'],
-            path: asset.resource
-          }
-          original = transloadit.step('original', '/s3/import', options)
-
-          options = {
-            use: "original",
-            preserve_meta_data: false
-          }
-          optimized = transloadit.step('optimized', '/image/optimize', options)
-          options = {
-            key: ENV['AWS_ACCESS_KEY'],
-            secret: ENV['AWS_SECRET_KEY'],
-            bucket: ENV['S3_BUCKET'],
-            path: storage_path + "optimized/#{asset.resource}",
-            use: "optimized"
-          }
-          store_optimized = transloadit.step('store_optimized', '/s3/store', options)
-
-          size = Image.new(asset.resource).size
-          options = {
-            use: "original",
-            strip: true,
-            width: (size[0] / 2),
-            height: (size[1] / 2)
-          }
-          resize = transloadit.step('resize', '/image/resize', options)
-          options = {
-            key: ENV['AWS_ACCESS_KEY'],
-            secret: ENV['AWS_SECRET_KEY'],
-            bucket: ENV['S3_BUCKET'],
-            path: storage_path + asset.resource,
-            use: "resize"
-          }
-          store_resize = transloadit.step('store_resize', '/s3/store', options)
-          options = {
-            key: ENV['AWS_ACCESS_KEY'],
-            secret: ENV['AWS_SECRET_KEY'],
-            bucket: ENV['S3_BUCKET'],
-            path: storage_path + "original/#{asset.resource}",
-            use: "original"
-          }
-          store_original = transloadit.step('store_original', '/s3/store', options)
-          transloadit_response = transloadit.assembly(steps: [original, optimized, resize, store_resize, store_optimized, store_original], notify_url: notify_url).submit!
-
-          asset.update_attributes(resource: storage_path + asset.resource, job_id: transloadit_response["assembly_id"])
-        else  # process audio with Transloadit
-          storage_path = "#{SecureRandom.urlsafe_base64(32)}/#{asset.resource}"
-
-          transloadit = Transloadit.new(
-            service: ENV['TRANSLOADIT_URL'],
-            key: ENV['TRANSLOADIT_AUTH_KEY'],
-            secret: ENV['TRANSLOADIT_SECRET_KEY']
-          )
-
-          options = {
-            key: ENV['AWS_ACCESS_KEY'],
-            secret: ENV['AWS_SECRET_KEY'],
-            bucket: ENV['S3_BUCKET_UPLOAD'],
-            path: asset.resource
-          }
-          original = transloadit.step('original', '/s3/import', options)
-
-          options = {
-            use: "original",
-            preset: "mp3"
-          }
-          encode = transloadit.step('encode', '/audio/encode', options)
-
-          options = {
-            key: ENV['AWS_ACCESS_KEY'],
-            secret: ENV['AWS_SECRET_KEY'],
-            bucket: ENV['S3_BUCKET'],
-            path: storage_path,
-            use: ["original","encode"]
-          }
-          store = transloadit.step('store', '/s3/store', options)
-
-          transloadit_response = transloadit.assembly(steps: [original, encode, store], notify_url: notify_url).submit!
-
-          asset.update_attributes(resource: storage_path, job_id: transloadit_response["assembly_id"])
-        end
+        media_proc = MediaProc.new(asset.resource)
+        proc_response = media_proc.process
 
         source_bucket.objects.delete(asset.resource)
+
+        asset.update_attributes(resource: media_proc.storage_path + asset.resource, job_id: proc_response["assembly_id"])
       else
         AssetWorker.perform_in(15.seconds, asset_id)
       end
--- lib/media_proc.rb
+++ lib/media_proc.rb
@@ -1,12 +1,14 @@
 class MediaProc
+  include Rails.application.routes.url_helpers
+
   attr_accessor :storage_path
 
-  def initialize(file_path, options = {})
-    @file_path = FileUri.new(file_path)
+  def initialize(resource, options = {})
+    @resource = resource
     @options = options
 
     @transloadit = initialize_transloadit
-    @storage_path = "#{SecureRandom.urlsafe_base64(32)}"
+    @storage_path = "#{SecureRandom.urlsafe_base64(32)}/"
   end
 
   def process
@@ -23,7 +25,7 @@ class MediaProc
   end
 
   def media_type
-   case @file_path.extension
+   case @resource[/[^.]*$/]
    when 'gif','jpg','jpeg','png'
      'image'
    when 'mp4','mov'
@@ -43,52 +45,45 @@ class MediaProc
     )
   end
 
-  def process_image_original
-
+  def process_image
+    # Optimize the original by compressing and removing meta data
     options = {
-      key: ENV['AWS_ACCESS_KEY'],
-      secret: ENV['AWS_SECRET_KEY'],
-      bucket: ENV['S3_BUCKET_UPLOAD'],
-      path: "#{@file_path.key}/#{@file_path.filename}"
+      use: "original",
+      preserve_meta_data: false
     }
-    original = @transloadit.step('original', '/s3/import', options)
-
-    options = {
-        use: "original",
-        width: 100,
-        height: 100,
-        strip: true
-    }
-    thumb = @transloadit.step('thumb', '/image/resize', options)
-
+    optimized = @transloadit.step('optimized', '/image/optimize', options)
+    # Store optimized version
     options = {
       key: ENV['AWS_ACCESS_KEY'],
       secret: ENV['AWS_SECRET_KEY'],
       bucket: ENV['S3_BUCKET'],
-      path: "#{@storage_path}/${previous_step.name}/${file.url_name}",
-      use: ["original", "thumb"]
+      path: storage_path + "optimized/#{@resource}",
+      use: "optimized"
     }
-    store = @transloadit.step('store', '/s3/store', options)
-
-    assembly = @transloadit.assembly(steps: [original, thumb, store])
-    assembly.submit!
-  end
-
-  def process_image
-    new_file_path = "https://#{@file_path.host}/#{ENV['S3_BUCKET']}/#{@storage_path}/#{@file_path.filename}"
-    s3_file = S3File.new @file_path
-    s3_file.move_to new_file_name
-  end
-
-  def process_audio
-
+    store_optimized = @transloadit.step('store_optimized', '/s3/store', options)
+    # Create an image with half the aspect ratio
+    size = Image.new(@resource).size
+    options = {
+      use: "original",
+      strip: true,
+      width: (size[0] / 2),
+      height: (size[1] / 2)
+    }
+    resize = @transloadit.step('resize', '/image/resize', options)
+    # Store the resized image
     options = {
       key: ENV['AWS_ACCESS_KEY'],
       secret: ENV['AWS_SECRET_KEY'],
-      bucket: ENV['S3_BUCKET_UPLOAD'],
-      path: "#{@file_path.key}/#{@file_path.filename}"
+      bucket: ENV['S3_BUCKET'],
+      path: storage_path + @resource,
+      use: "resize"
     }
-    original = @transloadit.step('original', '/s3/import', options)
+    store_resize = @transloadit.step('store_resize', '/s3/store', options)
+    # Execute the steps
+    submit_assembly [original, optimized, resize, store_resize, store_optimized, store_original]
+  end
+
+  def process_audio
 
     options = {
       use: "original",
@@ -100,18 +95,60 @@ class MediaProc
       key: ENV['AWS_ACCESS_KEY'],
       secret: ENV['AWS_SECRET_KEY'],
       bucket: ENV['S3_BUCKET'],
-      path: "#{@storage_path}/${previous_step.name}/${file.url_name}",
-      use: ["original", "thumb"]
+      path: "#{@storage_path}/#{@resource}",
+      use: "encode"
     }
-    store = @transloadit.step('store', '/s3/store', options)
+    store_encode = @transloadit.step('store', '/s3/store', options)
 
     assembly = @transloadit.assembly(steps: [original, encode, store])
     assembly.submit!
+    submit_assembly [original, encode, store_encode, store_original]
   end
 
   def process_video
-    new_file_path = "https://#{@file_path.host}/#{ENV['S3_BUCKET']}/#{@storage_path}/#{@file_path.filename}"
-    s3_file = S3File.new @file_path
-    s3_file.move_to new_file_name
+    # Doing a copy from the source bucket to the destination
+    s3 = AWS::S3.new(
+      access_key_id: ENV['AWS_ACCESS_KEY'],
+      secret_access_key: ENV['AWS_SECRET_KEY']
+    )
+    source_bucket = s3.buckets[ENV['S3_BUCKET_UPLOAD']]
+    dest_bucket = s3.buckets[ENV['S3_BUCKET']]
+    source_obj = source_bucket.objects[@resource]
+    dest_obj = dest_bucket.objects[storage_path]
+
+    source_obj.copy_to(dest_obj, { acl: :public_read })
   end
+
+  private
+
+    def notify_url
+      Rails.env == 'development' ? 'http://localhost/' : process_response_url(id, host: ENV['URL_HOST'], protocol: 'https')
+    end
+
+    def submit_assembly(steps)
+      @transloadit.assembly(steps: steps, notify_url: notify_url).submit!
+    end
+
+    def original
+      # Import the original for processing
+      options = {
+        key: ENV['AWS_ACCESS_KEY'],
+        secret: ENV['AWS_SECRET_KEY'],
+        bucket: ENV['S3_BUCKET_UPLOAD'],
+        path: @resource
+      }
+      @transloadit.step('original', '/s3/import', options)
+    end
+
+    def store_original
+      # Store the original
+      options = {
+        key: ENV['AWS_ACCESS_KEY'],
+        secret: ENV['AWS_SECRET_KEY'],
+        bucket: ENV['S3_BUCKET'],
+        path: storage_path + "original/#{@resource}",
+        use: "original"
+      }
+      @transloadit.step('store_original', '/s3/store', options)
+    end
 end
