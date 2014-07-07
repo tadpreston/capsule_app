073d8771aaf53353ccdb8bbd5204a2477819555f
Tad Preston <tpreston@udsamerica.com>
2014-07-07 09:51:55 -0500

    Reimplement ar serializer

--- app/serializers/capsule_serializer.rb
+++ app/serializers/capsule_serializer.rb
@@ -1,6 +1,6 @@
 class CapsuleSerializer < ActiveModel::Serializer
-# cached
-# delegate :cache_key, to: :object
+  cached
+  delegate :cache_key, to: :object
 
   attributes :id, :in_reply_to, :title, :location, :relative_location, :payload_type, :status, :promotional_state
   attributes :thumbnail_path, :start_date, :lock_question, :lock_answer
