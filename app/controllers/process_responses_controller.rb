0699e9f409605bd9306c598f506de46c6c66dc21
Tad Preston <tpreston@udsamerica.com>
2014-07-07 14:48:25 -0500

    Using strong params

--- app/controllers/process_responses_controller.rb
+++ app/controllers/process_responses_controller.rb
@@ -2,9 +2,9 @@ class ProcessResponsesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    job_id = params[:assembly_id]
+    job_id = transloadit_params[:assembly_id]
     asset = Asset.find_by(job_id: job_id)
-    asset.update_attributes(complete: true, metadata: params)
+    asset.update_attributes(complete: true, metadata: transloadit_params)
 
     s3 = AWS::S3.new(
       access_key_id: ENV['AWS_ACCESS_KEY'],
@@ -15,4 +15,10 @@ class ProcessResponsesController < ApplicationController
 
     render nothing: true
   end
+
+  private
+
+    def transloadit_params
+      params.required(:transloadit).permit!
+    end
 end
