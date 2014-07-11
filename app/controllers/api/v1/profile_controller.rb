8c6bbbe561070caad8c8ec4c1ba215f8fb249a11
Tad Preston <tpreston@udsamerica.com>
2014-07-11 12:08:03 -0500

    Limit the number of capsules to 2750

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -20,7 +20,7 @@ module API
       end
 
       def loadtest_jbuilder
-        @capsules = Capsule.all.limit(2000).includes(:user, :watchers, :read_by)
+        @capsules = Capsule.all.limit(2750).includes(:user, :watchers, :read_by)
       end
 
       private
