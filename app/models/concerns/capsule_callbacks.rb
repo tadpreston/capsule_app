e7a2a36bcda826f05cb7d1288b30d86f6d978da5
Tad Preston <tadpreston@gmail.com>
2014-07-21 15:31:43 -0500

    Location isn't required, just make sure it is there

--- app/models/concerns/capsule_callbacks.rb
+++ app/models/concerns/capsule_callbacks.rb
@@ -2,8 +2,10 @@ class CapsuleCallbacks
   def self.before_save(capsule)
     capsule.hash_tags = capsule.title.split(/\s+|[;,]\s*/).select { |v| v[0] == '#' }.join(' ')
 
-    capsule.latitude = capsule.location["latitude"] if capsule.location["latitude"]
-    capsule.longitude = capsule.location["longitude"] if capsule.location["longitude"]
+    if capsule.location
+      capsule.latitude = capsule.location["latitude"] if capsule.location["latitude"]
+      capsule.longitude = capsule.location["longitude"] if capsule.location["longitude"]
+    end
   end
 
   def self.after_save(capsule)
