bb853501796dd3ba59640ac2d652ecd190f1d307
Tad Preston <tadpreston@gmail.com>
2014-08-04 16:15:14 -0500

    Wrong number of params

--- app/workers/capsule_location_watch_worker.rb
+++ app/workers/capsule_location_watch_worker.rb
@@ -3,6 +3,6 @@ class CapsuleLocationWatchWorker
   include Sidekiq::Worker
 
   def perform(capsule_id, tenant_id = 1)
-    CapsuleLocationWatch.add_to_watched_locations(capsule_id, 1)
+    CapsuleLocationWatch.add_to_watched_locations(capsule_id)
   end
 end
