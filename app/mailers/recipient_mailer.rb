11bfe6c4730a73961fed45ef0935ae509bb2c674
Tad Preston <tadpreston@gmail.com>
2014-08-04 16:29:35 -0500

    Unscoped find of the capsule

--- app/mailers/recipient_mailer.rb
+++ app/mailers/recipient_mailer.rb
@@ -3,7 +3,7 @@ class RecipientMailer < ActionMailer::Base
 
   def capsule_notification(user_id, capsule_id)
     @user = User.find user_id
-    @capsule = Capsule.find capsule_id
+    @capsule = Capsule.unscoped.find capsule_id
 #   mail(to: @user.email, subject: "Welcome to Capsule! You have been sent a capsule.", content_type: "text/html")
     mail(to: 'tadpreston@gmail.com', subject: "Welcome to Capsule! You have been sent a capsule.", content_type: "text/html")
   end
