267a4f2eff7ceb1c3ac2040ad6fad63ebfcae2be
Tad Preston <tadpreston@gmail.com>
2014-12-19 06:06:02 -0600

    Added recipients

--- app/serializers/capsule_serializer.rb
+++ app/serializers/capsule_serializer.rb
@@ -3,6 +3,7 @@ class CapsuleSerializer < ActiveModel::Serializer
   delegate :cache_key, to: :object
 
   attributes :id, :in_reply_to, :comment, :location, :relative_location, :payload_type, :status, :promotional_state
+  attributes :id, :in_reply_to, :recipients, :location, :relative_location, :payload_type, :status, :promotional_state
   attributes :thumbnail_path, :start_date, :lock_question, :lock_answer
   attributes :likes_count, :comments_count, :created_at, :updated_at, :comments, :assets
 
