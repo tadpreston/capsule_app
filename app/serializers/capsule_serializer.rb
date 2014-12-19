a998baba994326f9014dcfa2ba399e07ee49caf3
Tad Preston <tadpreston@gmail.com>
2014-12-19 06:06:34 -0600

    Removed comments from the capsule payload

--- app/serializers/capsule_serializer.rb
+++ app/serializers/capsule_serializer.rb
@@ -2,16 +2,9 @@ class CapsuleSerializer < ActiveModel::Serializer
   cached
   delegate :cache_key, to: :object
 
-  attributes :id, :in_reply_to, :comment, :location, :relative_location, :payload_type, :status, :promotional_state
   attributes :id, :in_reply_to, :recipients, :location, :relative_location, :payload_type, :status, :promotional_state
   attributes :thumbnail_path, :start_date, :lock_question, :lock_answer
-  attributes :likes_count, :comments_count, :created_at, :updated_at, :comments, :assets
-
-  def comments
-    object.comments.map do |comment|
-      CommentSerializer.new comment, root: false
-    end
-  end
+  attributes :likes_count, :comments_count, :created_at, :updated_at, :assets
 
   def assets
     object.assets.map do |asset|
