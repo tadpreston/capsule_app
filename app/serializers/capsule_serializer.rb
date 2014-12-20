6040fa795e68982b337f14bf8957edf15194a9c6
Tad Preston <tadpreston@gmail.com>
2014-12-20 15:48:56 -0600

    Add creator and recipient serializers

--- app/serializers/capsule_serializer.rb
+++ app/serializers/capsule_serializer.rb
@@ -2,13 +2,23 @@ class CapsuleSerializer < ActiveModel::Serializer
   cached
   delegate :cache_key, to: :object
 
-  attributes :id, :in_reply_to, :recipients, :location, :relative_location, :payload_type, :status, :promotional_state
-  attributes :thumbnail_path, :start_date, :lock_question, :lock_answer
-  attributes :likes_count, :comments_count, :created_at, :updated_at, :assets
+  attributes :id, :comment, :creator, :recipients, :location, :relative_location, :payload_type, :status, :promotional_state
+  attributes :thumbnail_path, :assets, :start_date, :lock_question, :lock_answer
+  attributes :likes_count, :comments_count, :created_at, :updated_at
 
   def assets
     object.assets.map do |asset|
       AssetSerializer.new asset, root: false
+    end
+  end
+
+  def creator
+    CreatorSerializer.new object.user, root: false
+  end
+
+  def recipients
+    object.recipients.map do |recipient|
+      RecipientSerializer.new recipient, root: false
     end
   end
 end
---
+++ app/serializers/recipient_serializer.rb
@@ -0,0 +1,6 @@
+class RecipientSerializer < ActiveModel::Serializer
+  cached
+  delegate :cache_key, to: :object
+
+  attributes :id, :full_name, :phone_number, :email, :location, :profile_image_path, :created_at, :updated_at
+end
