4ce418fd750ed4bc2e0a3ee52aad445845566e97
Tad Preston <tadpreston@gmail.com>
2014-12-27 21:34:45 -0600

    New notification classes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,36 @@
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id                :integer          not null, primary key
+#  user_id           :integer
+#  capsule_id        :integer
+#  message           :text
+#  notification_type :text
+#  notified          :boolean
+#  created_at        :datetime
+#  updated_at        :datetime
+#
+
 class Notification < ActiveRecord::Base
+  NOTIFICATION_TYPES = [EMAIL = 'email', PUSH = 'push', SMS = 'sms']
+
   belongs_to :user
   belongs_to :capsule
+
+  delegate :device_token, to: :user, prefix: true
+
+  def deliver
+    send_push_notification if notification_type == PUSH
+    send_email_notification if notification_type == EMAIL
+  end
+
+  def send_push_notification
+    push_notification = PushNotification.new(user_device_token)
+    push_notification.push message
+  end
+
+  def send_email_notification
+    RecipientMailer.unlocked_capsule(user_id, capsule_id).deliver
+  end
 end
---
+++ app/models/notifications/unlock_notification.rb
@@ -0,0 +1,31 @@
+class Notifications::UnlockNotification
+  def initialization capsule
+    @capsule = capsule
+  end
+
+  def process
+    create_notification if @capsule.notifications.empty?
+  end
+
+  def create_notification
+    @capsule.recipients.each do |recipient|
+      send_notification
+    end
+  end
+
+  def send_notification recipient
+    notification = Notification.create capsule_id: @capsule.id,
+                                       user_id: recipient.id,
+                                       message: 'A Yada has been unlocked for you!',
+                                       message_type: message_type(recipient)
+    notification.deliver
+  end
+
+  def message_type recipient
+    if recipient.device_token.blank?
+      Notification::EMAIL
+    else
+      Notification::PUSH
+    end
+  end
+end
