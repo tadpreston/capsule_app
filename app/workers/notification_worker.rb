a4692e9f2360fe096655bdddc12282c3eae86866
Tad Preston <tadpreston@gmail.com>
2014-12-27 21:57:06 -0600

    Read capsules unscoped

--- app/workers/notification_worker.rb
+++ app/workers/notification_worker.rb
@@ -2,7 +2,7 @@ class NotificationWorker
   include Sidekiq::Worker
 
   def perform time
-    capsule_ids = Capsule.where('start_date < ?', time).ids
+    capsule_ids = Capsule.unscoped.where('start_date < ?', time).ids
     capsule_ids.each do |id|
       UnlockNotificationWorker.perform_async id
     end
--- app/workers/unlock_notification_worker.rb
+++ app/workers/unlock_notification_worker.rb
@@ -2,7 +2,7 @@ class UnlockNotificationWorker
   include Sidekiq::Worker
 
   def perform capsule_id
-    capsule = Capsule.find capsule_id
+    capsule = Capsule.unscoped.find capsule_id
     Notifications::UnlockCapsule.new(capsule).process
   end
 end
