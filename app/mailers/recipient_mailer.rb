24179b2ecfda5e6cebb754af4ec07c64a63f7583
Tad Preston <tadpreston@gmail.com>
2014-12-27 21:33:51 -0600

    New mailer for unlocked eiders

--- app/mailers/recipient_mailer.rb
+++ app/mailers/recipient_mailer.rb
@@ -1,10 +1,21 @@
 class RecipientMailer < ActionMailer::Base
-  default from: "capsule@capsulethemoment.com"
+  default from: "yada@pinyada.com"
 
   def capsule_notification(user_id, capsule_id)
+    get_user_and_capsule user_id, capsule_id
+    mail(to: @user.email, subject: "You have been sent a Yada.", content_type: "text/html")
+  end
+
+  def unlocked_capsule(user_id, capsule_id)
+    get_user_and_capsule user_id, capsule_id
+#   mail(to: @user.email, subject: "A Yada has been unlocked and is ready to you to view!", content_type: "text/html")
+    mail(to: 'tadpreston@gmail.com', subject: "A Yada has been unlocked and is ready to you to view!", content_type: "text/html")
+  end
+
+  private
+
+  def get_user_and_capsule user_id, capsule_id
     @user = User.find user_id
-    @capsule = Capsule.unscoped.find capsule_id
-#   mail(to: @user.email, subject: "Welcome to Capsule! You have been sent a capsule.", content_type: "text/html")
-    mail(to: 'tadpreston@gmail.com', subject: "Welcome to Capsule! You have been sent a capsule.", content_type: "text/html")
+    @capsule = Capsule.find capsule_id
   end
 end
