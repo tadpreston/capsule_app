3e318bcb70a6dff49890b958106398681ffb0ec2
Tad Preston <tadpreston@gmail.com>
2014-12-27 22:58:05 -0600

    Send to real users now

--- app/mailers/recipient_mailer.rb
+++ app/mailers/recipient_mailer.rb
@@ -8,8 +8,7 @@ class RecipientMailer < ActionMailer::Base
 
   def unlocked_capsule(user_id, capsule_id)
     get_user_and_capsule user_id, capsule_id
-#   mail(to: @user.email, subject: "A Yada has been unlocked and is ready to you to view!", content_type: "text/html")
-    mail(to: 'tadpreston@gmail.com', subject: "A Yada has been unlocked and is ready to you to view!", content_type: "text/html")
+    mail(to: @user.email, subject: "A Yada has been unlocked and is ready to you to view!", content_type: "text/html")
   end
 
   private
