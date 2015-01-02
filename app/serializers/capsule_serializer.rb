fe4b68916fb50492633f12e2fcd8cd7369953a3d
Tad Preston <tadpreston@gmail.com>
2015-01-01 21:28:33 -0600

    Added is_read to capsule serializer

--- app/serializers/capsule_serializer.rb
+++ app/serializers/capsule_serializer.rb
@@ -1,10 +1,11 @@
 class CapsuleSerializer < ActiveModel::Serializer
   cached
   delegate :cache_key, to: :object
+  delegate :current_user, to: :scope
 
   attributes :id, :comment, :creator, :recipients, :location, :relative_location, :payload_type, :status, :promotional_state
   attributes :thumbnail_path, :assets, :start_date, :lock_question, :lock_answer
-  attributes :likes_count, :comments_count, :created_at, :updated_at
+  attributes :likes_count, :comments_count, :is_read, :created_at, :updated_at
 
   def assets
     object.assets.map do |asset|
@@ -20,5 +21,9 @@ class CapsuleSerializer < ActiveModel::Serializer
     object.recipients.map do |recipient|
       RecipientSerializer.new recipient, root: false
     end
+  end
+
+  def is_read
+    object.read_by?(current_user)
   end
 end
