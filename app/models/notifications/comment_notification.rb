94ffa4491c63c054c4b1ab1636d20a125589ddbd
Tad Preston <tadpreston@gmail.com>
2015-01-08 21:44:45 -0600

    Notifications to Notification

--- app/models/notifications/comment_notification.rb
+++ app/models/notifications/comment_notification.rb
@@ -1,6 +1,6 @@
 class Notifications::CommentNotification < Notifications::Base
   def process msg
-    @notification_type = Notifications::NEW_COMMENT
+    @notification_type = Notification::NEW_COMMENT
     create_notification msg
   end
 
