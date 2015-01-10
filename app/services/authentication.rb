3b8b9b9c4a614fb1021a4f4737929d2a35094529
Tad Preston <tadpreston@gmail.com>
2015-01-10 16:48:26 -0600

    Set the mode when authenticated

--- app/services/authentication.rb
+++ app/services/authentication.rb
@@ -9,24 +9,37 @@ class Authentication
   def authenticated?
     if @user && @user.authenticate(@params[:password])
       @user.reload
-      device = @user.devices.current_device(@request.remote_ip, @request.user_agent)
-      if device
-        device.last_sign_in_at = Time.now
-        device.reset_auth_token!
-      else
-        device = @user.devices.create(remote_ip: @request.remote_ip, user_agent: @request.user_agent, last_sign_in_at: Time.now)
-      end
+      update_or_create_device
+      set_mode
       @user
     end
   end
 
   private
 
-    def get_user(params)
-      if params[:email]
-        User.find_by(email: params[:email])
-      elsif params[:oauth]
-        Users::Search.find_or_create_by_oauth(params[:oauth])
-      end
+  def get_user(params)
+    if params[:email]
+      User.find_by(email: params[:email])
+    elsif params[:oauth]
+      Users::Search.find_or_create_by_oauth(params[:oauth])
     end
+  end
+
+  def update_or_create_device
+    device = @user.devices.current_device(@request.remote_ip, @request.user_agent)
+    if device
+      device.last_sign_in_at = Time.now
+      device.reset_auth_token!
+    else
+      @user.devices.create(remote_ip: @request.remote_ip, user_agent: @request.user_agent, last_sign_in_at: Time.now)
+    end
+  end
+
+  def set_mode
+    if @params[:test]
+      @user.update_attribute :mode, 'test'
+    else
+      @user.update_attribute :mode, ''
+    end
+  end
 end
