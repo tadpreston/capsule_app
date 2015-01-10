f960e9a82979ec55ccb2120ee37434b1fd9ced6c
Tad Preston <tadpreston@gmail.com>
2015-01-10 15:41:17 -0600

    Change the client to Apns

--- app/services/push_notification.rb
+++ app/services/push_notification.rb
@@ -2,7 +2,7 @@ class PushNotification
 
   def initialize device_token
     raise ArgumentError, 'Device token cannot be blank' if device_token.blank?
-    @client = Client.new device_token
+    @client = ApnsClient.new device_token
   end
 
   def push message, badge=1
