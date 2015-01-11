a5d6c3cc9584d557416342001c9350d7991331f4
Tad Preston <tadpreston@gmail.com>
2015-01-11 11:21:42 -0600

    Fix error in client

--- app/services/apns_client.rb
+++ app/services/apns_client.rb
@@ -3,6 +3,7 @@ class ApnsClient
     raise ArgumentError, 'Device token cannot be blank' if device_token.blank?
     @file = ApnsCert.new(mode).file
     @device_token = device_token
+    @mode = mode
     initialize_client
   end
 
@@ -15,7 +16,7 @@ class ApnsClient
   def initialize_client
     APNS.pem = @file
     APNS.host = ENV['APNS_HOST']
-    APNS.host = ENV['APNS_HOST_DEV'] if mode == 'test'
+    APNS.host = ENV['APNS_HOST_DEV'] if @mode == 'test'
     APNS.port = 2195
   end
 
