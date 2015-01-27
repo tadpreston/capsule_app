372415b5989627db52ddbec538a2cd9dbb70e082
Drew Wyatt <drew.j.wyatt@gmail.com>
2015-01-26 23:19:18 -0600

    Added untested changes that may allow Notification to pass capsule properties down the stack to PushNotification and eventually the apns_client.

--- app/services/apns_client.rb
+++ app/services/apns_client.rb
@@ -1,14 +1,15 @@
 class ApnsClient
-  def initialize device_token, mode=''
+  def initialize device_token, mode='', other_data={}
     raise ArgumentError, 'Device token cannot be blank' if device_token.blank?
     @file = ApnsCert.new(mode).file
     @device_token = device_token
     @mode = mode
+    @other_data=other_data
     initialize_client
   end
 
   def push message, badge=1
-    APNS.send_notification @device_token, alert: message, badge: badge, sound: 'yada.wav'
+    APNS.send_notification @device_token, alert: message, badge: badge, sound: 'yada.wav', other: @other
   end
 
   private
