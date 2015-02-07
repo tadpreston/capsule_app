44e806d501bfce6c3d99964e74622d04080abfee
Tad Preston <tadpreston@gmail.com>
2015-02-07 11:44:05 -0600

    profile_image_path to profile_image

--- app/serializers/creator_serializer.rb
+++ app/serializers/creator_serializer.rb
@@ -2,5 +2,9 @@ class CreatorSerializer < ActiveModel::Serializer
 # cached
 # delegate :cache_key, to: :object
 
-  attributes :id, :full_name, :phone_number, :email, :location, :profile_image_path, :created_at, :updated_at
+  attributes :id, :full_name, :phone_number, :email, :location, :profile_image, :created_at, :updated_at
+
+  def profile_image
+    object.profile_image_path
+  end
 end
