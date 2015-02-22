21eb5f8de4cd9dc72e526e6c4d5ebc3fd9ee38b6
Tad Preston <tadpreston@gmail.com>
2015-02-22 13:45:42 -0600

    remove authentication

--- app/controllers/api/v1/upgrades_controller.rb
+++ app/controllers/api/v1/upgrades_controller.rb
@@ -1,6 +1,8 @@
 module API
   module V1
     class UpgradesController < API::V1::ApplicationController
+      skip_before_action :authorize_auth_token
+
       def index
         render json: { force_upgrade: false }
       end
