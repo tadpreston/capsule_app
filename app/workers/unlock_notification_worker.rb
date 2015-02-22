e1cd7ad750f9ca61f0c564e3836ab9458fc6bdf6
Tad Preston <tadpreston@gmail.com>
2015-02-21 20:22:58 -0600

    No need to use unscoped. Not using tenant

--- app/workers/unlock_notification_worker.rb
+++ app/workers/unlock_notification_worker.rb
@@ -3,7 +3,7 @@ class UnlockNotificationWorker
   sidekiq_options :queue => :time_yada
 
   def perform capsule_id
-    capsule = Capsule.unscoped.find capsule_id
+    capsule = Capsule.find capsule_id
     Notifications::UnlockNotification.new(capsule).process
   end
 end
