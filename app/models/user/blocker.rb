5d67f6d81e89cb880e988581cee40044ad36e930
Tad Preston <tadpreston@gmail.com>
2015-03-11 07:49:46 -0500

    Remove yada from the feed

--- app/models/user/blocker.rb
+++ app/models/user/blocker.rb
@@ -7,6 +7,7 @@ class User::Blocker < User
   def block_user phone_number
     user = set_user phone_number
     Block.create_block id, user.id
+    remove_from_feeds user.id
   end
 
   def remove_block phone_number
@@ -20,5 +21,14 @@ class User::Blocker < User
     user = User::Blocker.find_by phone_number: phone_number
     raise ActiveRecord::RecordNotFound, 'User not found by phone number' unless user
     user
+  end
+
+  def remove_from_feeds user_id
+    relevances = Relevance.where capsule_id: yada_ids_from(user_id), user_id: id
+    relevances.destroy_all
+  end
+
+  def yada_ids_from user_id
+    relevant_yadas.where(user_id: user_id).ids
   end
 end
