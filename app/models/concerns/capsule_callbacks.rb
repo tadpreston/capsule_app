64bdaac8f19bc42059cae62507d1690c895b9210
Tad Preston <tadpreston@gmail.com>
2015-03-11 21:30:51 -0500

    Don't add to the feed if the user is blocked

--- app/models/concerns/capsule_callbacks.rb
+++ app/models/concerns/capsule_callbacks.rb
@@ -11,12 +11,13 @@ class CapsuleCallbacks
 
     unless capsule.recipients_attributes.nil?
       recipients = capsule.recipients_attributes
+      author = User::Blocker.find capsule.user_id
 
       recipients.each do |recipient|
         user = Users::Search.find_or_create_recipient(recipient)
         capsule.add_as_recipient user
         capsule.user.add_as_contact user
-        unless user.id == capsule.user_id
+        unless user.id == capsule.user_id || author.is_blocked_by(user)
           capsule.relevances.create user_id: user.id, relevant_date: DateTime.now
         end
       end
