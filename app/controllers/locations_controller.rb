1110619579d73502b59b511bd651395c402c3bca
Tad Preston <tadpreston@gmail.com>
2015-04-29 21:06:33 -0500

    refactor of locations map

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,5 @@
 class LocationsController < ApplicationController
   def map
-    @locations = Capsule.location.map { |cap| {name: cap.location["name"], latitude: cap.location["latitude"], longitude: cap.location["longitude"]} }
-    @timeYadaCount = Capsule.where(location: nil).count
-    @totalYadaCount = Capsule.count
+    @yada_location = YadaLocation.new
   end
 end
