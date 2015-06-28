68d6402f5ee5cd08351f8298048e51125687d1b4
Tad Preston <tadpreston@gmail.com>
2015-06-28 16:31:05 -0500

    Add route for token access yada

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
   end
 
   namespace :api, defaults: {format: 'json'} do
+    resources :yadas, only: [:show]
+
     namespace :v1 do
       get 'config', to: 'configs#index'
       get 'search', to: 'searches#index'
