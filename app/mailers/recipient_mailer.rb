bc71cd348fdddb2942c155a2161ee6f5c0fca8aa
Tad Preston <tadpreston@gmail.com>
2015-06-29 08:08:50 -0500

    Clean up the wording

--- app/mailers/recipient_mailer.rb
+++ app/mailers/recipient_mailer.rb
@@ -8,7 +8,7 @@ class RecipientMailer < ActionMailer::Base
 
   def unlocked_capsule(user_id, capsule_id)
     get_user_and_capsule user_id, capsule_id
-    mail(to: @user.email, subject: "A Yada has been unlocked and is ready to you to view!", content_type: "text/html")
+    mail(to: @user.email, subject: "A Yada has been unlocked and is ready for you to view!", content_type: "text/html")
   end
 
   def new_capsule user_id, capsule_id, message
