0cf933e0716f03354687e724f5dcd3b1fab27987
Tad Preston <tadpreston@gmail.com>
2015-07-03 15:03:44 -0500

    Namespace was missing

--- app/serializers/api/v1a/registered_user_serializer.rb
+++ app/serializers/api/v1a/registered_user_serializer.rb
@@ -1,7 +1,11 @@
-class RegisteredUserSerializer < ActiveModel::Serializer
-  attributes *RegisteredUser::ATTRIBUTES
+module API
+  module V1a
+    class RegisteredUserSerializer < ActiveModel::Serializer
+      attributes *RegisteredUser::ATTRIBUTES
 
-  def profile_image
-    object.signed_profile_image
+      def profile_image
+        object.signed_profile_image
+      end
+    end
   end
 end
