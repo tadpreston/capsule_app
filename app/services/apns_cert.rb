97f2803c2d3d14386a321f54c6114a667ada9c0f
Tad Preston <tadpreston@gmail.com>
2015-07-13 20:45:01 -0500

    Use the new AWS gem because other was failing

--- app/services/apns_cert.rb
+++ app/services/apns_cert.rb
@@ -1,42 +1,33 @@
 class ApnsCert
+  attr_accessor :s3, :local_filename, :pem_filename
+
+  EAST_BUCKET = 'capsule-app-internal'
+
   def initialize mode
-    @s3 = AWS::S3.new(
-      access_key_id: ENV['AWS_ACCESS_KEY'],
-      secret_access_key: ENV['AWS_SECRET_KEY']
-    )
+    @s3 = Aws::S3::Client.new region: 'us-east-1', access_key_id: ENV['AWS_ACCESS_KEY_ID_EAST'],
+                                     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY_EAST']
     if mode == 'test'
       @pem_filename = ENV['APNS_PEM_FILE_NAME_DEV']
-      @local_filename = "#{ENV['APNS_PEM_FILE_NAME_DEV']}"
+      @local_filename = Rails.root.join('tmp','apns',ENV['APNS_PEM_FILE_NAME_DEV'])
     else
       @pem_filename = ENV['APNS_PEM_FILE_NAME']
-      @local_filename = "#{ENV['APNS_PEM_FILE_NAME']}"
+      @local_filename = Rails.root.join('tmp','apns',ENV['APNS_PEM_FILE_NAME'])
     end
   end
 
   def file
     get_file_from_remote
-    File.new @local_filename
+    File.new local_filename
   end
 
   def delete_file
-    File.delete @local_filename
+    File.delete local_filename
   end
 
   private
 
-  def bucket
-    @bucket ||= @s3.buckets[ENV['APNS_BUCKET']]
-  end
-
-  def file_object
-    @file_object ||= bucket.objects[@pem_filename]
-  end
-
   def get_file_from_remote
-    File.open(@local_filename, 'wb') do |file_out|
-      file_object.read do |chunk|
-        file_out.write(chunk)
-      end
-    end
+    FileUtils.mkdir_p Rails.root.join('tmp','apns')
+    s3.get_object bucket: EAST_BUCKET, response_target: local_filename, key: pem_filename
   end
 end
