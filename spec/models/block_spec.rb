ec2089b7ab8fcf6d998c2b613aed5728c8e69f54
Tad Preston <tadpreston@gmail.com>
2015-10-13 21:46:01 -0500

    Annotated the models

--- spec/models/block_spec.rb
+++ spec/models/block_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: blocks
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  blocked_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Block do
