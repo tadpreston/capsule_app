ce1e001308b281f81a0b4aa798c6589e70fff022
Tad Preston <tadpreston@gmail.com>
2015-10-13 21:05:05 -0500

    A mandrill service object

---
+++ app/services/mandrill_mailer.rb
@@ -0,0 +1,45 @@
+class MandrillMailerRejectedError < StandardError; end
+class MandrillMailer
+  attr_reader :email, :subject, :from_name, :template_name
+
+  def initialize email, subject, from_name, template_name
+    @email = email
+    @subject = subject
+    @from_name = from_name
+    @template_name = template_name
+  end
+
+  def self.deliver email:, subject:, from_name:, template_name: ENV['DEFAULT_MANDRILL_TEMPLATE']
+    new(email, subject, from_name, template_name).deliver
+  end
+
+  def deliver
+    result = mandrill.messages.send message
+    raise MandrillMailerRejectedError, error_message(result.first)  unless result.first['status'] == 'sent'
+    result
+  end
+
+  private
+
+  def message
+    {
+      subject: subject,
+      from_name: from_name,
+      from_email: ENV['DEFAULT_FROM_EMAIL'],
+      to: [ { email: email } ],
+      html: template.fetch("html")
+    }
+  end
+
+  def mandrill
+    @mandrill ||= Mandrill::API.new
+  end
+
+  def template options=[]
+    mandrill.templates.render template_name, options
+  end
+
+  def error_message result
+    "#{result['status']} - #{result['reject_reason']}"
+  end
+end
