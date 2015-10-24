5559f0fe4572d1905a6296d8bcea0ac1d6fca061
Tad Preston <tadpreston@gmail.com>
2015-10-24 14:36:07 -0500

    Update the worker for the new schema

--- app/workers/welcome_email_worker.rb
+++ app/workers/welcome_email_worker.rb
@@ -3,7 +3,7 @@ class WelcomeEmailWorker
 
   def perform user_id
     result = WelcomeMailer.deliver(email: user(user_id).email, subject: 'Welcome To PinYada!', from_name: 'PinYada').first
-    Mandrill.create email: user.email, status: result.status, message_id: result.message_id, reason: result.reject_reason
+    MandrillResult.create user_id: user_id, status: result.status, message_id: result.message_id, reason: result.reject_reason
   end
 
   private
