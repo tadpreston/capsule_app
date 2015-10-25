d0d7aa752bc8ac1c3215edd587e6e3654f87a332
Tad Preston <tadpreston@gmail.com>
2015-10-25 15:44:57 -0500

    Updated the migration to create the categories

--- db/migrate/20151025180651_create_categories.rb
+++ db/migrate/20151025180651_create_categories.rb
@@ -5,5 +5,9 @@ class CreateCategories < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    ['Birthday', 'Good Morning', 'Good Night', 'Good Luck', 'Congratulations', 'Love', 'Sorry', 'Happy Thanksgiving', 'Merry Christmas', 'Happy New Year'].each do |name|
+      Category.create name: name
+    end
   end
 end
