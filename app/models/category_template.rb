75628f7111dbb67a486f64592f13eda57f669cbb
Tad Preston <tadpreston@gmail.com>
2015-10-31 09:57:28 -0500

    Annotated models

--- app/models/category_template.rb
+++ app/models/category_template.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: category_templates
+#
+#  id          :integer          not null, primary key
+#  category_id :integer
+#  template_id :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class CategoryTemplate < ActiveRecord::Base
   belongs_to :category
   belongs_to :template
