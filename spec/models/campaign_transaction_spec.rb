885bdf306c63ccd7943d9a182c6290c6c06f72b4
Tad Preston <tadpreston@gmail.com>
2015-11-02 21:54:23 -0600

    Update the annotation

--- spec/models/campaign_transaction_spec.rb
+++ spec/models/campaign_transaction_spec.rb
@@ -2,13 +2,14 @@
 #
 # Table name: campaign_transactions
 #
-#  id         :integer          not null, primary key
-#  capsule_id :integer
-#  user_id    :integer
-#  order_id   :string(255)
-#  amount     :decimal(, )
-#  created_at :datetime
-#  updated_at :datetime
+#  id          :integer          not null, primary key
+#  campaign_id :integer
+#  capsule_id  :integer
+#  user_id     :integer
+#  order_id    :string(255)
+#  amount      :decimal(, )
+#  created_at  :datetime
+#  updated_at  :datetime
 #
 
 require 'spec_helper'
