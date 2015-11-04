345655225ba9ad27f7676168bec874b7692bf106
Drew Wyatt <drew.j.wyatt@gmail.com>
2015-11-04 00:21:37 -0600

    sending emails using post params

--- app/controllers/api/v1a/campaigns_controller.rb
+++ app/controllers/api/v1a/campaigns_controller.rb
@@ -4,7 +4,12 @@ module API
       skip_before_action :authorize_auth_token
 
       def redeem
-        render json: StarbucksCard.redeem("Drew", "drewbuysstuff@gmail.com")
+        if(params[:email] && params[:yada_id])
+          yada = Capsule.find params[:yada_id]
+          render json: StarbucksCard.redeem(yada.user.username || "Friend", params[:email])
+        else
+          render json: { "success" => "bad params" }
+        end
       end
     end
   end
