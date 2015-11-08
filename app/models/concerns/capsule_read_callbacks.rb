13ea73de59fcec32f8c4c241affc508bcae62029
Tad Preston <tadpreston@gmail.com>
2015-11-08 15:49:57 -0600

    This needs to not create a one if it doesn't exist

--- app/models/concerns/capsule_read_callbacks.rb
+++ app/models/concerns/capsule_read_callbacks.rb
@@ -1,9 +1,7 @@
 class CapsuleReadCallbacks
   def self.after_create capsule_read
     CapsuleReadNotificationWorker.perform_in 1.second, capsule_read.id
-    relevance = Relevance.find_or_create_by capsule_id: capsule_read.capsule_id, user_id: capsule_read.capsule.user_id do |r|
-      r.relevant_date = Time.current
-    end
-    relevance.update_attributes relevant_date: Time.current
+    relevance = Relevance.find_by capsule_id: capsule_read.capsule_id, user_id: capsule_read.capsule.user_id
+    relevance.update_attributes relevant_date: Time.current if relevance
   end
 end
