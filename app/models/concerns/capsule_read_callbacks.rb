ba09358f08fc51c6bdd3cc1d2fe0887c4ddab542
Tad Preston <tadpreston@gmail.com>
2015-11-08 14:10:39 -0600

    Use find_or_create

--- app/models/concerns/capsule_read_callbacks.rb
+++ app/models/concerns/capsule_read_callbacks.rb
@@ -1,12 +1,9 @@
 class CapsuleReadCallbacks
   def self.after_create capsule_read
     CapsuleReadNotificationWorker.perform_in 1.second, capsule_read.id
-    relavance(capsule_read.capsule).update_attributes relevant_date: DateTime.now
-  end
-
-  private
-  
-  private_class_method def self.relavance capsule
-    Relevance.where(capsule_id: capsule.id, user_id: capsule.user_id).first
+    relevance = Relevance.find_or_create_by capsule_id: capsule_read.capsule_id, user_id: capsule_read.capsule.user_id do |r|
+      r.relevant_date = Time.current
+    end
+    relevance.update_attributes relevant_date: Time.current
   end
 end
