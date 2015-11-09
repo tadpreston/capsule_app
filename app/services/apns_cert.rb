bac8fce33a76c37fae609f46fb8f60ca8f9df37d
Tad Preston <tadpreston@gmail.com>
2015-11-09 07:15:40 -0600

    Look to the west

--- app/services/apns_cert.rb
+++ app/services/apns_cert.rb
@@ -1,11 +1,11 @@
 class ApnsCert
   attr_accessor :s3, :local_filename, :pem_filename
 
-  EAST_BUCKET = 'capsule-app-internal'
+  WEST_BUCKET = 'pinyada'
 
   def initialize mode
-    @s3 = Aws::S3::Client.new region: 'us-east-1', access_key_id: ENV['AWS_ACCESS_KEY_ID_EAST'],
-                                     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY_EAST']
+    @s3 = Aws::S3::Client.new region: 'us-west-2', access_key_id: ENV['AWS_ACCESS_KEY_ID_WEST'],
+                                     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY_WEST']
     if mode == 'test'
       @pem_filename = ENV['APNS_PEM_FILE_NAME_DEV']
       @local_filename = Rails.root.join('tmp','apns',ENV['APNS_PEM_FILE_NAME_DEV'])
@@ -28,6 +28,6 @@ class ApnsCert
 
   def get_file_from_remote
     FileUtils.mkdir_p Rails.root.join('tmp','apns')
-    s3.get_object bucket: EAST_BUCKET, response_target: local_filename, key: pem_filename
+    s3.get_object bucket: WEST_BUCKET, response_target: local_filename, key: "assets/certificates/#{pem_filename}"
   end
 end
