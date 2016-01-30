8a5da8b96a775f6c9c956ff259ded30247dec04f
Tad Preston <tadpreston@gmail.com>
2016-01-30 14:40:20 -0600

    This is an api so don't need a layout

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,7 +2,6 @@ class Admin::ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  layout "admin/layouts/application"
 
   before_action :authorize
 
