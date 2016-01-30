69bcb7ceda8f2b8cd3108291dca306ae0767fc79
Tad Preston <tadpreston@gmail.com>
2016-01-30 12:54:39 -0600

    Refactor of some clumsy code

--- app/services/apns_cert.rb
+++ app/services/apns_cert.rb
@@ -1,18 +1,10 @@
 class ApnsCert
-  attr_accessor :s3, :local_filename, :pem_filename
+  attr_reader :mode
 
   WEST_BUCKET = 'pinyada'
 
   def initialize mode
-    @s3 = Aws::S3::Client.new region: 'us-west-2', access_key_id: ENV['AWS_ACCESS_KEY_ID_WEST'],
-                                     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY_WEST']
-    if mode == 'test'
-      @pem_filename = ENV['APNS_PEM_FILE_NAME_DEV']
-      @local_filename = Rails.root.join('tmp','apns',ENV['APNS_PEM_FILE_NAME_DEV'])
-    else
-      @pem_filename = ENV['APNS_PEM_FILE_NAME']
-      @local_filename = Rails.root.join('tmp','apns',ENV['APNS_PEM_FILE_NAME'])
-    end
+    @mode = mode
   end
 
   def file
@@ -29,5 +21,22 @@ class ApnsCert
   def get_file_from_remote
     FileUtils.mkdir_p Rails.root.join('tmp','apns')
     s3.get_object bucket: WEST_BUCKET, response_target: local_filename, key: "assets/certificates/#{pem_filename}"
+  end
+
+  def s3
+    @s3 ||= Aws::S3::Client.new region: 'us-west-2', access_key_id: ENV['AWS_ACCESS_KEY_ID_WEST'],
+                                secret_access_key: ENV['AWS_SECRET_ACCESS_KEY_WEST']
+  end
+
+  def pem_filename
+    if mode == 'test'
+      ENV['APNS_PEM_FILE_NAME_DEV']
+    else
+      ENV['APNS_PEM_FILE_NAME']
+    end
+  end
+
+  def local_filename
+    Rails.root.join('tmp','apns', pem_filename)
   end
 end
