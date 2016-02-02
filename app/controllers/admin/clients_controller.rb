411368c377d33e7ddd1d4ce4785d4598083c6378
Tad Preston <tadpreston@gmail.com>
2016-02-01 20:25:24 -0600

    Complete the client controller

--- app/controllers/admin/clients_controller.rb
+++ app/controllers/admin/clients_controller.rb
@@ -1,25 +1,36 @@
 class Admin::ClientsController < Admin::ApplicationController
+  before_action :find_client, only: [:show, :update, :destroy]
+
   def index
     clients = Client.all
     render json: clients, each_serializer: Admin::ClientSerializer
   end
 
   def show
-    client = Client.find params[:id]
-    render json: client, serializer: Admin::ClientSerializer
+    @client = Client.find params[:id]
+    render json: @client, serializer: Admin::ClientSerializer
   end
 
   def create
-    @client = Client.new client_params.merge(created_by: current_user.id, updated_by: current_user.id)
-    if @client.save
-      render json: @client, serializer: API::V1a::ClientSerializer, status: 201
+    client = Client.new client_params.merge(created_by: current_user.id, updated_by: current_user.id)
+    if client.save
+      render json: client, serializer: Admin::ClientSerializer, status: 201
     else
       render_client_errors
     end
   end
 
   def update
+    if @client.update_attributes(client_params.merge(updated_by: current_user.id))
+      render json: @client, serializer: Admin::ClientSerializer
+    else
+      render_client_errors
+    end
+  end
 
+  def destroy
+    @client.destroy
+    render json: { status: 'Deleted' }
   end
 
   private
@@ -38,5 +49,9 @@ class Admin::ClientsController < Admin::ApplicationController
         path: request.env['REQUEST_PATH']
       ]
     }
+  end
+
+  def find_client
+    @client = Client.find params[:id]
   end
 end
