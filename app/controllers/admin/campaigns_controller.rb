3681d74afae9710536e90ee783debb74092a4d3c
Tad Preston <tadpreston@gmail.com>
2016-02-03 08:58:01 -0600

    Add the create method

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -10,6 +10,15 @@ class Admin::CampaignsController < Admin::ApplicationController
     render json: @campaign, serializer: Admin::CampaignSerializer
   end
 
+  def create
+    campaign = Campaign.new client_params.merge(created_by: current_user.id, updated_by: current_user.id)
+    if campaign.save
+      render json: campaign, serializer: Admin::CampaignSerializer, status: 201
+    else
+      render_campaign_errors
+    end
+  end
+
   private
 
   def find_client
@@ -18,5 +27,22 @@ class Admin::CampaignsController < Admin::ApplicationController
 
   def find_campaign
     @campaign = @client.campaigns.find params[:id]
+  end
+
+  def campaign_params
+    params.required(:campaign).permit :name, :description, :budget, :client_message, :user_message, :image_from_client, :image_from_user,
+                                      :image_keep, :image_forward, :image_expired
+  end
+
+  def render_campaign_errors
+    render status: 400, json: {
+      errors: [
+        status: '400',
+        code: '400',
+        title: @client.errors,
+        links: [],
+        path: request.env['REQUEST_PATH']
+      ]
+    }
   end
 end
